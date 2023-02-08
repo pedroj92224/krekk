70378395e877ccf97327b5e832c845fd90380fd0
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 12:45:54 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -11,15 +11,15 @@ url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distan
 col_dtypes = {'County': str}
 col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
 
-df = pd.read_csv(url, usecols=['County'] + city_columns, dtype=col_dtypes)
+df = pd.read_csv(url, dtype=col_dtypes)
 city_columns = [col for col in df.columns if col != 'County']
 city_names = [col.replace("_", " ") for col in city_columns]
 columnz = st.selectbox("Choose a city", city_names)
-column_index = city_columns.index(columnz.replace(" ", "_"))
+columnz = city_columns[city_names.index(columnz)]
 numby = st.slider('Select a mile radius', 0, 500)
 
-df = df.loc[df[city_columns[column_index]] <= numby]
-df = df.sort_values(by=[city_columns[column_index]])
+df = df.loc[df[columnz] <= numby]
+df = df.sort_values(by=[columnz])
 df = df.drop_duplicates(subset=['County'], keep='first')
-df = df[['County', city_columns[column_index]]]
-st.dataframe(df)
+df = df[['County', columnz]]
+df
