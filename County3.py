1c7943c540120c7908d1258f1bbd92a11eae968e
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 13:06:20 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -8,19 +8,23 @@ st.subheader('Choose a city:')
 
 url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distances_Offline.csv'
 
-col_dtypes = {'County': str}
-col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
+city_columns = []
+city_names = []
+for chunk in pd.read_csv(url, chunksize=100):
+    city_columns = [col for col in chunk.columns if col != 'County']
+    city_names = [col.replace("_", " ") for col in city_columns]
+    break
 
-city_columns = None
-for chunk in pd.read_csv(url, dtype=col_dtypes, chunksize=100):
-    if city_columns is None:
-        city_columns = [col for col in chunk.columns if col != 'County']
-        city_names = [col.replace("_", " ") for col in city_columns]
-        columnz = st.selectbox("Choose a city", city_names)
-        columnz = city_columns[city_names.index(columnz)]
-        numby = st.slider('Select a mile radius', 0, 500)
-    chunk = chunk.loc[chunk[columnz] <= numby]
-    chunk = chunk.sort_values(by=[columnz])
-    chunk = chunk.drop_duplicates(subset=['County'], keep='first')
-    chunk = chunk[['County', columnz]]
-    st.write(chunk)
+columnz = st.selectbox("Choose a city", city_names)
+columnz_index = city_columns.index(columnz.replace(" ", "_"))
+numby = st.slider('Select a mile radius', 0, 500)
+
+df_result = pd.DataFrame(columns=['County', columnz])
+for chunk in pd.read_csv(url, usecols=['County', city_columns[columnz_index]], chunksize=100):
+    df = chunk.loc[chunk[city_columns[columnz_index]] <= numby]
+    df = df.sort_values(by=[city_columns[columnz_index]])
+    df = df.drop_duplicates(subset=['County'], keep='first')
+    df = df[['County', city_columns[columnz_index]]]
+    df_result = pd.concat([df_result, df], ignore_index=True)
+
+df_result
