3aa6e09c52b3d07f544f0c81a1ce31836771d83b
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 15:49:14 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -7,17 +7,26 @@ st.subheader('Choose a city:')
 
 url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distances_Offline.csv'
 
-col_dtypes = {'County': str}
-col_dtypes.update({str(col): 'Int32' for col in range(1, 5270)})
+city_columns = None
+city_names = None
 
-df = pd.read_csv(url, dtype=col_dtypes)
-city_columns = [col for col in df.columns if col != 'County']
-city_names = [col.replace("_", " ") for col in city_columns]
+# Read only the first chunk of the CSV file to get the city columns
+for chunk in pd.read_csv(url, chunksize=1000):
+    if city_columns is None:
+        city_columns = [col for col in chunk.columns if col != 'County']
+        city_names = [col.replace("_", " ") for col in city_columns]
+        break
+
 columnz = st.selectbox("Choose a city", city_names)
 numby = st.slider('Select a mile radius', 0, 500)
 
-df2 = df[['County', columnz]]
-df3 = df2[df2[columnz] <= numby]
-df3 = df3.sort_values(by=[columnz])
-df3 = df3.drop_duplicates(subset=['County'], keep='first')
-st.write(df3)
+# Read only the selected city column and the County column, with the dtype of the city column set to int32
+df = pd.read_csv(url, usecols=['County', columnz], dtype={columnz: np.int32})
+
+# Filter the rows where the value of the selected city column is less than or equal to the selected radius
+df = df.loc[df[columnz] <= numby]
+df = df.sort_values(by=[columnz])
+df = df.drop_duplicates(subset=['County'], keep='first')
+df = df[['County', columnz]]
+
+st.write(df)
