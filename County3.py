27a7044954aeca104be775e605aa8a7282ef1be7
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 13:15:53 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -8,14 +8,22 @@ st.subheader('Choose a city:')
 
 url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distances_Offline.csv'
 
-city_names = [col.replace("", " ") for col in pd.read_csv(url, nrows=0).columns if col != 'County']
-columnz = st.selectbox("Choose a city", city_names)
-columnz = columnz.replace(" ", "")
+# get the column names
+with open(url) as f:
+    first_line = f.readline()
+column_names = first_line.strip().split(',')
+
+columnz = st.selectbox("Choose a city", column_names[1:])
 numby = st.slider('Select a mile radius', 0, 500)
 
+# set usecols based on user input
 usecols = ['County', columnz]
-df_chunk = pd.read_csv(url, usecols=usecols, dtype={'County': str, columnz: np.int32}, chunksize=10**5)
-df = pd.concat([chunk.loc[chunk[columnz] <= numby] for chunk in df_chunk], ignore_index=True)
+
+# read the csv in chunks
+df_chunks = pd.read_csv(url, usecols=usecols, chunksize=100000, dtype={'County': str, columnz: np.int32})
+df = pd.concat([chunk for chunk in df_chunks])
+
+df = df.loc[df[columnz] <= numby]
 df = df.sort_values(by=[columnz])
 df = df.drop_duplicates(subset=['County'], keep='first')
 df = df[['County', columnz]]
