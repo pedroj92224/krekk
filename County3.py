e45f045e9025a464482b767d6caecf6287f51aa3
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 14:32:05 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -13,17 +13,16 @@ col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
 
 city_columns = None
 
-columnz = st.selectbox("Choose a city", city_names)
-numby = st.number_input('Select a mile radius', min_value=0, max_value=500)
-
 for chunk in pd.read_csv(url, dtype=col_dtypes, chunksize=1000):
     if city_columns is None:
         city_columns = [col for col in chunk.columns if col != 'County']
         city_names = [col.replace("_", " ") for col in city_columns]
 
-    df = chunk.loc[chunk[columnz] <= numby]
-    df2 = df.sort_values(by=[columnz])
-    df3 = df2.drop_duplicates(subset=['County'], keep='first')
-    df4 = df3[['County', columnz]]
+columnz = st.selectbox("Choose a city", city_names)
+numby = st.slider('Select a mile radius', 0, 500)
 
-st.write(df4)
+df = chunk.loc[chunk[columnz] <= numby]
+df = df.sort_values(by=[columnz])
+df = df.drop_duplicates(subset=['County'], keep='first')
+df = df[['County', columnz]]
+st.write(df)
