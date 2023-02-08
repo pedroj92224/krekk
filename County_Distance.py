cd539b7df4e7fa6f469390e65fd39f6e72ef5c49
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 10:36:01 -0800

    Update County_Distance.py

--- County_Distance.py
+++ County_Distance.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from collections import defaultdict
+
 
 st.set_page_config(page_title='Distance Tool')
 st.header('Counties Within Mile Radius')
