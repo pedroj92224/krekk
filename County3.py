a4d5581d66b358e01d2b802db60ea2c82a541d70
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-08 16:25:51 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -7,21 +7,20 @@ st.header('Counties Within Mile Radius')
 st.subheader('Choose a city:')
 
 url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distances_Offline.csv'
-
 col_dtypes = {'County': str}
 col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
-
 city_columns = None
 
 for chunk in pd.read_csv(url, dtype=col_dtypes, chunksize=1000):
     if city_columns is None:
         city_columns = [col for col in chunk.columns if col != 'County']
         city_names = [col.replace("_", " ") for col in city_columns]
+        break
 
 columnz = st.selectbox("Choose a city", city_names)
 numby = st.slider('Select a mile radius', 0, 500)
-
-df = chunk.loc[chunk[columnz] <= numby]
+df = pd.read_csv(url, usecols=['County', columnz], dtype=col_dtypes)
+df = df.loc[df[columnz] <= numby]
 df = df.sort_values(by=[columnz])
 df = df.drop_duplicates(subset=['County'], keep='first')
 df = df[['County', columnz]]
