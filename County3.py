c1e768421f2db00a01f207c47d4e7a9fc00dd446
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-09 09:34:30 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -9,25 +9,21 @@ st.subheader('Choose a city:')
 url = 'https://media.githubusercontent.com/media/pedroj92224/krekk/master/Distances_Offline.csv'
 
 col_dtypes = {'County': str}
-for i in range(1, 5270):
-    col_dtypes[str(i)] = np.int32
+col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
 
 city_columns = None
-city_names = None
 
-def load_data(city_name, numby):
-    chunk = pd.read_csv(url, chunksize=500, dtype=col_dtypes, usecols=['County', city_name])
-    df = chunk.loc[chunk[city_name] <= numby]
-    df = df.sort_values(by=[city_name])
-    df = df.drop_duplicates(subset=['County'], keep='first')
-    df = df[['County', city_name]]
-    return df
+for chunk in pd.read_csv(url, dtype=col_dtypes, chunksize=1000):
+    if city_columns is None:
+        city_columns = [col for col in chunk.columns if col != 'County']
+        city_names = [col.replace("_", " ") for col in city_columns]
+        break
 
-if city_columns is None:
-    city_columns = pd.read_csv(url, nrows=0).columns
-    city_names = [col.replace("_", " ") for col in city_columns if col != 'County']
-    city_name = st.selectbox("Choose a city", city_names)
-    numby = st.slider('Select a number', 0, 500, step=1, format='%d')
+columnz = st.selectbox("Choose a city", city_names)
+numby = st.slider('Select a mile radius', 0, 500)
 
-result = load_data(city_name, numby)
-st.write(result)
+df = chunk.loc[chunk[columnz] <= numby]
+df = df.sort_values(by=[columnz])
+df = df.drop_duplicates(subset=['County'], keep='first')
+df = df[['County', columnz]]
+st.write(df)
