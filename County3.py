cfd161c267d6ae13e64f51eb523f7fa150a47cf5
pedroj92224 <123713078+pedroj92224@users.noreply.github.com>
2023-02-09 09:10:24 -0800

    Update County3.py

--- County3.py
+++ County3.py
@@ -14,10 +14,9 @@ col_dtypes.update({str(col): np.int32 for col in range(1, 5270)})
 city_columns = None
 city_names = None
 
-@st.cache
 def load_data(city_name, numby):
-    chunk = pd.read_csv(url, chunksize=1000, dtype=col_dtypes, usecols=['County', city_name])
-    df = chunk.loc[chunk[city_name] <= numby]
+    df = pd.read_csv(url, chunksize=1000, dtype=col_dtypes, usecols=['County', city_name])
+    df = df.loc[df[city_name] <= numby]
     df = df.sort_values(by=[city_name])
     df = df.drop_duplicates(subset=['County'], keep='first')
     df = df[['County', city_name]]
